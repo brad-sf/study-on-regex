56adb791249b1c5cd6a957a1525a42439051951a
Bradley Flood <bradley.flood@gmail.com>
2017-04-11 13:58:44 +1000

    Quantifier

--- regex-training.js
+++ regex-training.js
@@ -6,14 +6,14 @@ str = `Mouse house`;
 rgx = new RegExp("house");
 rgx = /house/;
 
-// RegExp .test
+// RegExp .test returns bool
 console.log(rgx.test(str)); // true
 
-// `g` global flag
+// `g` global flag search entire string
 rgx = new RegExp("use", "g");
 rgx = /use/g;
 
-// .exec
+// .exec state aware (loops to start after null)
 console.log(rgx.exec(str)); // [ 'use', index: 2, input: 'Mouse house' ]
 console.log(rgx.exec(str)); // [ 'use', index: 8, input: 'Mouse house' ]
 console.log(rgx.exec(str)); // null
@@ -24,19 +24,34 @@ str = `Rat bag brat`;
 rgx = new RegExp("rat", "gi");
 rgx = /rat/gi;
 
-// String .match
+// String .match returns array
 console.log(str.match(rgx)); // [ 'Rat', 'rat' ]
 
 // String .replace
 console.log(str.replace(rgx, str => `zzz`)); // zzz bag bzzz
 
 // Simple console log matcher
-const match = (st,rx) => console.log(st.match(rx,st).join());
+const match = (st,rx) => console.log(st.match(rx,st));
 
-// . special char
-str = `Red bed`;
+// `.` special char matches any single character
+str = `Red bed.`;
 rgx = /.ed/g;
-match(str, rgx); // Red,bed
+match(str, rgx); // [ 'Red', 'bed' ]
 
-rgx = /.{3}/g;
-match(str, rgx); // Red, be
+rgx = /.../g;
+match(str, rgx); // [ 'Red', ' be' ]
+
+// `\` escape character
+rgx = /\./g;
+match(str, rgx); // [ '.' ]
+
+// `{n}` quantifier
+str = `aaaaa`;
+rgx = /a{3}/g;
+match(str, rgx); // [ 'aaa' ]
+
+// `{min, max}` quantifier
+rgx = /a{2,5}/g;
+match(str, rgx); // [ 'aaaaa' ]
+
+
